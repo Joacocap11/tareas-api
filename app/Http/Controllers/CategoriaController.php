7bb66e7bbd495aae526cf8b8cc606d9351e2b3d0
Joaquin Trujillo <joaquin.d.trujillo@gmail.com>
2025-06-23 10:27:36 -0300

    Agrego categorias al CRUD

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    public function index()
+    {
+        return Categoria::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|unique:categorias,nombre|max:100'
+        ]);
+
+        $categoria = Categoria::create([
+            'nombre' => $request->nombre
+        ]);
+
+        return response()->json($categoria, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $categoria = Categoria::findOrFail($id);
+
+        $request->validate([
+            'nombre' => 'required|string|unique:categorias,nombre,' . $categoria->id
+        ]);
+
+        $categoria->nombre = $request->nombre;
+        $categoria->save();
+
+        return response()->json($categoria);
+    }
+
+    public function destroy($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->delete();
+
+        return response()->json(['message' => 'Categoría eliminada']);
+    }
+}
+
+?>
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\TareaController;
+use App\Http\Controllers\CategoriaController;
 
 Route::apiResource('tareas', TareaController::class);
-
+Route::apiResource('categorias', CategoriaController::class)->except(['show']);
 
 
 ?>
